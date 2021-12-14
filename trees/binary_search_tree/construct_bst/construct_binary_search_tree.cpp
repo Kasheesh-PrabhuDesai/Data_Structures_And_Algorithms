022f00b2314517716e7368ae5187302957ebac98
Kasheesh PrabhuDesai <kasheesh171995@gmail.com>
2021-12-14 15:25:27 +0530

    construct bst from given level order array

--- trees/binary_search_tree/construct_bst/construct_binary_search_tree.cpp
+++ trees/binary_search_tree/construct_bst/construct_binary_search_tree.cpp
@@ -98,7 +98,7 @@ void printLevelOrder(BinarySearchTree<int>* root)
 
 int main()
 {
-    vector<int> a = {1,2,3,4,5,6,7,8};
+    vector<int> a = {5,3,6,2,4,7};
 
     BinarySearchTree<int>* root = NULL;
 
